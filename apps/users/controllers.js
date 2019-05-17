0c92d35d7cd6636b2aebf6de6394bffaf3dadafe
David J. Leon <djleona@unal.edu.co>
2019-05-17 09:46:52 -0500

    cambiando estructura

--- apps/users/controllers.js
+++ apps/users/controllers.js
@@ -1,23 +1,185 @@
 'use strict';
-const express = require('express'),
-  router = express.Router(),
-  Manager = require('./models').Manager,
-  Employee = require('./models').Employee;
+const express = require('express');
+const mongoose = require('mongoose');
 
-router.route('/')
+const Manager = mongoose.model("managers");
+const Employee = mongoose.model("employees");
 
-    .get(function (req, res) {
-        Manager.find()
-        .then(function (managers) {
-            res.locals.managers = managers;
-            return res.render('managers/managers.html');
+module.exports = app => {
+  app.get("/managers", (req, res) => {
+    const query = req.query;
+    Manager.find(query)
+      .then(managers => {
+        res.json({
+          confirmation: "success",
+          data: managers
         });
-    });
-
-    .get(function (req, res) {
-        Employee.find()
-        .then(function (employees) {
-            res.locals.employees = employees;
-            return res.render('employees/employees.html');
+      })
+      .catch(err => {
+        res.json({
+          confirmation: "fail",
+          message: err.message
         });
-    });
+      });
+  });
+
+  app.get("/managers/add", (req, res) => {
+    const data = req.query;
+    new Manager(data)
+      .save()
+      .then(manager => {
+        res.json({
+          confirmation: "success",
+          data: manager
+        });
+      })
+      .catch(err => {
+        res.json({
+          confirmation: "fail",
+          message: err.message
+        });
+      });
+  });
+
+  app.get("/managers/update/:id", (req, res) => {
+    const updateDetails = req.query;
+    const idManager = req.params.idManager;
+
+    Manager.findByIdAndUpdate(idManager, updateDetails, { new: true })
+      .then(manager => {
+        res.json({
+          confirmation: "success",
+          data: manager
+        });
+      })
+      .catch(err => {
+        res.json({
+          confirmation: "fail",
+          message: err.message
+        });
+      });
+  });
+
+  app.get("/managers/delete/:id", (req, res) => {
+    const idManager = req.params.idManager;
+
+    Manager.findByIdAndDelete(idManager)
+      .then(manager => {
+        res.json({
+          confirmation: "success",
+          data: manager
+        });
+      })
+      .catch(err => {
+        res.json({
+          confirmation: "fail",
+          message: err.message
+        });
+      });
+  });
+
+  app.get("/managers/:id", (req, res) => {
+    Manager.findById(req.params.idManager)
+      .then(manager => {
+        res.json({
+          confirmation: "success",
+          data: manager
+        });
+      })
+      .catch(err => {
+        res.json({
+          confirmation: "fail",
+          message: err.message
+        });
+      });
+  });
+
+  app.get("/employees", (req, res) => {
+    const query = req.query;
+    Employee.find(query)
+      .then(employees => {
+        res.json({
+          confirmation: "success",
+          data: employees
+        });
+      })
+      .catch(err => {
+        res.json({
+          confirmation: "fail",
+          message: err.message
+        });
+      });
+  });
+
+  app.get("/employees/add", (req, res) => {
+    const data = req.query;
+    new Employee(data)
+      .save()
+      .then(employee => {
+        res.json({
+          confirmation: "success",
+          data: employee
+        });
+      })
+      .catch(err => {
+        res.json({
+          confirmation: "fail",
+          message: err.message
+        });
+      });
+  });
+
+  app.get("/employees/update/:id", (req, res) => {
+    const updateDetails = req.query;
+    const idEmployee = req.params.idEmployee;
+
+    Employee.findByIdAndUpdate(idEmployee, updateDetails, { new: true })
+      .then(employee => {
+        res.json({
+          confirmation: "success",
+          data: employee
+        });
+      })
+      .catch(err => {
+        res.json({
+          confirmation: "fail",
+          message: err.message
+        });
+      });
+  });
+
+  app.get("/employees/delete/:id", (req, res) => {
+    const idEmployee = req.params.idEmployee;
+
+    Employee.findByIdAndDelete(idEmployee)
+      .then(employee => {
+        res.json({
+          confirmation: "success",
+          data: employee
+        });
+      })
+      .catch(err => {
+        res.json({
+          confirmation: "fail",
+          message: err.message
+        });
+      });
+  });
+
+  app.get("/employees/:id", (req, res) => {
+    Employee.findById(req.params.idEmployee)
+      .then(employee => {
+        res.json({
+          confirmation: "success",
+          data: manager
+        });
+      })
+      .catch(err => {
+        res.json({
+          confirmation: "fail",
+          message: err.message
+        });
+      });
+  });
+
+};
--- apps/users/schemas.js
+++ apps/users/schemas.js
@@ -1,23 +1,21 @@
-'use strict';
-const mongoose = require('../../config/mongoose'),
-	  Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const schemas = {
+const managerSchema: new Schema({
+		idManager: {type: Number, default:0}
+		userName: {type: String, default:""}
+		password: {type: String, default:""}
+		name: {type:String, default:""}
+		restaurant: {type: String,default:""}
+	});
 
-	managerSchema: new Schema({
-		idManager: {type: Number}
-		userName: {type: String}
-		password: {type: String}
-		name: {type:String}
-		restaurant: {type: String}
+const employeeSchema: new Schema({
+		idEmployee: {type: Number, default:0}
+		name: {type: String, default:""}
+		office: {type: String, default:""}
 	})
 
-	employeeSchema: new Schema({
-		idEmployee: {type: Number}
-		name: {type: String}
-		office: {type: String}
-	})
 
-};
 
-module.exports = schemas;
+mongoose.model('managers',managerSchema);
+mongoose.model('employees',managerSchema);
